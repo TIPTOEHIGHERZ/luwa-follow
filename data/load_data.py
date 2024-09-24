ae55869ff57d034aab3db36cc5fd5fcd3ca0a0a0
Tiptoe <zhangzeyutiptoe@gmail.com>
2024-09-24 21:15:08 +0800

    able to load per batch from disk

--- data/load_data.py
+++ data/load_data.py
@@ -37,11 +37,11 @@ class BatchLoader:
 
         self.meta_data = pd.read_csv(os.path.join(data_path, 'LUWA', 'CSV',
                                                   f'{reso}_{mag}_{size}_{train}.csv'))
-        picture_path = os.path.join(data_path,
-                                    'LUWA',
-                                    f'{self.reso}',
-                                    f'{self.mag}',
-                                    f'{self.modality}')
+        self.picture_path = os.path.join(data_path,
+                                         'LUWA',
+                                         f'{self.reso}',
+                                         f'{self.mag}',
+                                         f'{self.modality}')
 
         self.image_list = self.meta_data.iloc[:, 0].to_list()
         self.label_list = self.meta_data.iloc[:, 1].to_list()
@@ -49,38 +49,47 @@ class BatchLoader:
         self.batch_idx = -1
         self.total_len = len(self.image_list) // batch_size + 1
 
-        self.images = list()
-        trans = transforms.ToTensor()
-        for i in range(len(self.image_list)):
-            image = Image.open(os.path.join(picture_path, f'{self.label_list[i]}'.lower(), self.image_list[i]))
-            self.images.append(trans(image).unsqueeze(0))
-        self.images = torch.cat(self.images, dim=0)
+        # shuffle the data
+        shuffle_idx = torch.randperm(len(self.image_list))
+        # print(self.image_list)
+        self.image_list = [self.image_list[idx] for idx in shuffle_idx]
+        self.label_list = [self.label_list[idx] for idx in shuffle_idx]
 
         labels_dict = list2dict(self.label_list)
         self.labels = torch.tensor([labels_dict[l] for l in self.label_list])
 
-        shuffle_idx = torch.randperm(self.images.shape[0])
-        self.images = self.images[shuffle_idx]
-        self.labels = self.labels[shuffle_idx]
 
         return
+
+    def get_batch(self, batch_idx):
+        indices = range(batch_idx * self.batch_size, min((batch_idx + 1) * self.batch_size, len(self.image_list)))
+
+        labels = self.labels[indices]
+        image_names = [self.image_list[idx] for idx in indices]
+
+        images = list()
+        trans = transforms.ToTensor()
+        for i in range(len(image_names)):
+            image = Image.open(os.path.join(self.picture_path, f'{self.label_list[i]}'.lower(), image_names[i]))
+            image = trans(image).unsqueeze(0)
+            images.append(image)
+
+        images = torch.cat(images, dim=0)
+
+        return images, labels
+
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        self.batch_idx += 1
-
-        if self.batch_idx >= self.total_len:
-            self.batch_idx = -1
-            raise StopIteration
-
-        idx = range(self.batch_idx * self.batch_size, min((self.batch_idx + 1) * self.batch_size, self.images.shape[0]))
-        return self.images[idx], self.labels[idx]
+        pass
 
     def __len__(self):
         return self.total_len
 
 
 if __name__ == '__main__':
-    batch_loader = BatchLoader()
+    batch_loader = BatchLoader(batch_size=32)
+    im, la = batch_loader.get_batch(0)
+    print(im.shape)
